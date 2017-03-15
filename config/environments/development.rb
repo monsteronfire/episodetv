f69b434f314204a012630cbabce8cbcfa3622e5a
monsteronfire <monsteronfire@gmail.com>
2017-03-15 15:51:27 +0800

    Add mailer settings to dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Sidekiq ActiveJob
   config.active_job.queue_adapter = :sidekiq
@@ -60,5 +59,14 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {}
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "episodes.com",
+    authentication: "plain",
+    enable_starttle_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
