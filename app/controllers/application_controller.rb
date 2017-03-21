b9a4f4aba8b7d2b0c0dbb7229222e08a5d85323e
monsteronfire <monsteronfire@gmail.com>
2017-03-21 13:43:17 +0800

    Hide subscribe to watch button for subscribed users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+
+  def current_user_subscribed?
+    user_signed_in? && current_user.subscribed?
+  end
+
+  helper_method :current_user_subscribed?
 end
